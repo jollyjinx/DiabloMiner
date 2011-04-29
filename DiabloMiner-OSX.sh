139c1dee4b3644c8c4af2833e785623ab8a0bfda
Patrick Stein <Patrick.Stein@tinkerbell.jinx.de>
2011-04-29 18:15:27 +0200

    changed to build the project when the target does not exist

--- DiabloMiner-OSX.sh
+++ DiabloMiner-OSX.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+if [ ! -d target ];
+then 
+    	echo Building Project
+	mvn package
+fi
+
 export GPU_USE_SYNC_OBJECTS=1
 java -Xmx16m -cp target/libs/*:target/DiabloMiner-0.0.1-SNAPSHOT.jar -Djava.awt.headless=true -Djava.library.path=target/libs/natives/macosx com.diablominer.DiabloMiner.DiabloMiner $@
